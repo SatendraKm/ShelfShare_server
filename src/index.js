f8e936dbb390e457a71edb4628f0b4b87fc677fb
Satendra Kumar <satendrakm27@gmail.com>
2025-04-12 01:25:35 +0530

    staging changes

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 const port = process.env.PORT || 5000;
